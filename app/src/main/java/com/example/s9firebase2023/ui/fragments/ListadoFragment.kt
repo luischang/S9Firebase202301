9ad23d700c715e027267562406f88e6330203c33
Luis Chang <luis.chang@outlook.com>
2023-06-03 11:19:54 -0500

    Added PrincipalActivity with Navigation Drawer

---
+++ app/src/main/java/com/example/s9firebase2023/ui/fragments/ListadoFragment.kt
@@ -0,0 +1,18 @@
+package com.example.s9firebase2023.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.s9firebase2023.R
+
+class ListadoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_listado, container, false)
+    }
+}
